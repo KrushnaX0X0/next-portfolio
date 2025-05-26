3431432450cdf524fc1d0c915fdff02884789ff2
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2025-05-26 11:53:55 +0530

    add onclick events

--- components/Gallaryopner.jsx
+++ components/Gallaryopner.jsx
@@ -33,7 +33,7 @@ function Gallaryopner({ setOpenerGallary }) {
               <img
                 src={item.url.src}
                 alt={item.description}
-                className="w-full h-full object-cover rounded overflow-hidden  "
+                className="w-full h-full object-cover rounded overflow-hidden "
                 onClick={() => {
                   setcurrentimg(item.url.src)
                   setcurrentdesc(item.description)
@@ -56,17 +56,40 @@ export default Gallaryopner
 
 
 const OpenOneImage = ({ currentimg ,setopenimg}) => {
+  
+     const [redHeart, setRedHeart] = useState(true)
+     const [infoOpner,setinfoOpner] = useState(false)
+
   openAnimation("box")
   return (
     <>
       <div className={`box h-0 w-0 absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]  ${Style.glass} `}>
-        { <img src={currentimg} alt="Selected" className="w-full h-full object-cover rounded overflow-hidden" /> }
+        { <img src={currentimg} alt="Selected" className="w-full h-full object-cover rounded overflow-hidden p-5" /> }
       </div>
-
       <div className=' cursor-pointer text-3xl bg-red-400 absolute  top-0 right-0 p-0' onClick={() =>setopenimg(false)}><i className="ri-close-fill"></i></div>
+          <div className={`h-12 ${Style.glass} absolute w-full bottom-0 right-0 flex items-center justify-end gap-4 text-white`}>
+                    <span className='ml-4 text-[22px] '><i className=" cursor-pointer text-gray-800 ri-delete-bin-6-line"></i></span>
+                    <span className='ml-4 text-[22px] text-black' onClick={()=>setinfoOpner(true)}><i className=" cursor-pointer ri-information-line"></i></span>
+                    {
+                        redHeart ? 
+                        <span className='ml-4 text-[22px] text-black ' onClick={()=>{setRedHeart(false)}}><i className=" cursor-pointer ri-heart-line" ></i></span> :
+                        <span className='ml-4 text-[22px] text-black' onClick={()=>{setRedHeart(true)}}><i className=" cursor-pointer ri-heart-fill text-red-600" ></i></span>
 
+                    }
+
+              <div className='h-full w-full '>
+                {
+                  infoOpner && 
+                  <div className='h-30 w-40 bg-amber-300 absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] p-2'>
+
+                    </div>
+                }
+              </div>
+
+
+               </div>
     </>
 
   )
-}
+} 
 
