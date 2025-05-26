59f7b7342b6711e8cbb9284c9059f68cd4c0942a
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2025-05-26 11:08:37 +0530

    add img screen opner

--- components/Gallaryopner.jsx
+++ components/Gallaryopner.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Style from "@/style/main.module.css"
 import { openAnimation, closeAnimation } from '@/app/lib/animationfun'
 import Shdows from './Shdows'
 import information from '@/app/lib/data'
 
 
+
 function Gallaryopner({ setOpenerGallary }) {
+
+  const [currentimg, setcurrentimg] = useState("")
+  const [currentdesc, setcurrentdesc] = useState("")
+  const [openimg, setopenimg] = useState(false)
+
   const handleClick = () => {
     setOpenerGallary(false)
 
@@ -15,13 +21,11 @@ function Gallaryopner({ setOpenerGallary }) {
   openAnimation("box")
 
   return (
-    <div className={`box h-0 w-0 absolute top-[48%] left-[52%] translate-y-[-50%] translate-x-[-50%]  ${Style.glass} bg-red-700`}>
+    <div className={`box h-0 w-0 absolute top-[48%] left-[52%] translate-y-[-50%] translate-x-[-50%]  ${Style.glass} `}>
       <div className='w-72 h-60 absolute bottom-0 right-0'>
         <Shdows color1='red' color2='purple' intencity={100} />
       </div>
       <div className='float-right cursor-pointer text-3xl bg-rose-400' onClick={() => handleClick()}><i className="ri-close-fill"></i></div>
-
-
       <div className="  h-full w-full  overflow-y-scroll scrollbar-hide gap-4 ">
         {information.map((block, index) =>
           block.photo.map((item, i) => (
@@ -30,15 +34,39 @@ function Gallaryopner({ setOpenerGallary }) {
                 src={item.url.src}
                 alt={item.description}
                 className="w-full h-full object-cover rounded overflow-hidden  "
+                onClick={() => {
+                  setcurrentimg(item.url.src)
+                  setcurrentdesc(item.description)
+                  setopenimg(true)
+                }}
               />
-              
             </div>
           ))
         )}
       </div>
+            {
+                 openimg && <OpenOneImage currentimg={currentimg} setopenimg={setopenimg}/>
+           }
 
     </div>
   )
 }
 
 export default Gallaryopner
+
+
+const OpenOneImage = ({ currentimg ,setopenimg}) => {
+  openAnimation("box")
+  return (
+    <>
+      <div className={`box h-0 w-0 absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]  ${Style.glass} `}>
+        { <img src={currentimg} alt="Selected" className="w-full h-full object-cover rounded overflow-hidden" /> }
+      </div>
+
+      <div className=' cursor-pointer text-3xl bg-red-400 absolute  top-0 right-0 p-0' onClick={() =>setopenimg(false)}><i className="ri-close-fill"></i></div>
+
+    </>
+
+  )
+}
+
